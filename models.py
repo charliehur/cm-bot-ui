6cf8d7d2d87d84e74ae433438df90254bd973585
charliehur <charlie@howurun.com>
2023-08-28 11:24:21 -0700

    changed temps and context

--- models.py
+++ models.py
@@ -37,8 +37,8 @@ def load_retriever():
     metadata_field_info = []
     #vector_store = Pinecone.from_existing_index(index_name=os.environ["PINECONE_INDEX_NAME"], embedding=OpenAIEmbeddings())
     vector_store = Pinecone.from_documents(data, embeddings, index_name=index_name)
-    llm = OpenAI(model_name="gpt-4", temperature=1.25, logit_bias={"8237":-100, "47553": -100,  "4462": -100})
-    document_content_description = "lyric content and lyric style"
+    llm = OpenAI(model_name="gpt-4", temperature=0.2)
+    document_content_description = "Colonize Mars user guide and cooking terms"
 
     return SelfQueryRetriever.from_llm(
         llm, vector_store, document_content_description, metadata_field_info
@@ -57,7 +57,7 @@ def load_chain():
         template=template
     )
 
-    llm = OpenAI(model_name="gpt-4", temperature=0, logit_bias={"8237": -100, "47553": -100, "4462": -100})
+    llm = OpenAI(model_name="gpt-4", temperature=0.4)
     #llm.logit_bias = {"8237":-100, "47553": -100}
 
     return load_qa_chain(llm, verbose=True, chain_type="stuff", prompt=prompt)
